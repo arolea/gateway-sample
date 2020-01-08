0055cad5ec9d2933ac12652881a43b8469b80810
alexandru.rolea <alexandru.rolea@softvision.ro>
2020-01-08 17:49:39 +0200

    Documentation

--- gateway-service/src/main/java/com/rolea/learning/gateway/config/GatewayConfig.java
+++ gateway-service/src/main/java/com/rolea/learning/gateway/config/GatewayConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 
+/**
+ * Route - an id, associated with a collection of predicates and a collection of filters
+ * Predicate - used to match routes, can be chained
+ * Filters - used to modify requests and responses, can be cached
+ */
 @Configuration
 public class GatewayConfig {
 
